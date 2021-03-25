53d28e9089a8a030d6c8506cd571aa6383fdc121
Brian M. Adams <briadam@sandia.gov>
2021-03-25 17:51:26 -0600

    Patch Acro/COLIN AppResponse for Clang 10
    DP-528: With thanks to Yuri on dakota-users for the initial report on
    FreeBSD, fix const issues in AppResponse::response_map_t (and
    response_pair_t).

--- acro/packages/colin/src/colin/AppResponse.h
+++ acro/packages/colin/src/colin/AppResponse.h
@@ -54,10 +54,10 @@ class AppResponse
 public: // typedefs
 
    ///
-   typedef std::pair<response_info_t, const utilib::Any >  response_pair_t;
+   typedef std::pair<response_info_t, utilib::Any >  response_pair_t;
 
    ///
-   typedef std::map<response_info_t, const utilib::Any >  response_map_t;
+   typedef std::map<response_info_t, utilib::Any >  response_map_t;
 
 public: // construct / destruct / copy / compare methods
 
