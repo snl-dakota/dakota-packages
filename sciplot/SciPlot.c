22a283a729e585a2d4358d66aeaa90979ad7a35e
Brian M. Adams <briadam@sandia.gov>
2022-02-03 10:30:47 -0700

    Motif/SciPlot: Fix Clang 12 errors with missing returns
    With thanks to Yuri for the report, fix a couple missing return values
    in utility functions.
    
    While this code is deprecated, the fix was straightforward.

--- sciplot/SciPlot.c
+++ sciplot/SciPlot.c
@@ -3761,7 +3761,7 @@ realpair* SciPlotListReturnPtr (Widget wi, int idnum, int* num_xy_pairs)
   SciPlotList *p;
   SciPlotWidget w;
   if (!XtIsSciPlot(wi))
-    return;
+    return NULL;
   w = (SciPlotWidget) wi;
 
   p = _ListFind(w, idnum);
@@ -3792,7 +3792,7 @@ Boolean SciPlotQuickUpdateCheck (Widget wi)
 {
   SciPlotWidget w;
   if (!XtIsSciPlot(wi))
-    return;
+    return False;
   w = (SciPlotWidget) wi;
 
   return CheckMinMax(w);
