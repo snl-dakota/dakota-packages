a10da408daadd5b911acbc9b28ff11e401812cc3
J. Adam Stephens <jasteph@sandia.gov>
2026-04-12 17:02:37 -0600

    Fix boost graph test in muq

--- muq2/cmake/checks/CheckBoost.cmake
+++ muq2/cmake/checks/CheckBoost.cmake
@@ -1,21 +1,29 @@
-# make sure that the boost graph library is available
-set(CMAKE_REQUIRED_LIBRARIES ${BOOST_LIBRARY})
+# make sure that the Boost graph headers are usable with the active compiler.
+#
+# find_package(Boost COMPONENTS system graph) already verifies that the
+# requested Boost libraries exist.  Re-linking a test executable here can fail
+# spuriously on some platforms due to transitive/system library handling, which
+# makes MUQ fall back to downloading and rebuilding Boost unnecessarily.
+set(CMAKE_REQUIRED_LIBRARIES "")
 set(CMAKE_REQUIRED_INCLUDES ${BOOST_INCLUDE_DIR})
 set(CMAKE_REQUIRED_FLAGS "${CMAKE_CXX_FLAGS}")
+
 CHECK_CXX_SOURCE_COMPILES(
 "
 #include <boost/graph/adjacency_list.hpp>
-typedef boost::adjacency_list < boost::vecS, boost::vecS, boost::bidirectionalS, int, int> Graph;
-int main(){
-Graph temp;
-return 0;
+
+typedef boost::adjacency_list<boost::vecS, boost::vecS,
+                              boost::bidirectionalS, int, int> Graph;
+
+int main() {
+  Graph temp;
+  return 0;
 }
 "
 BOOST_GRAPH_COMPILES)
 
-
 if(NOT BOOST_GRAPH_COMPILES)
-	set(BOOST_TEST_FAIL 1)
+  set(BOOST_TEST_FAIL 1)
 else()
-	set(BOOST_TEST_FAIL 0)
+  set(BOOST_TEST_FAIL 0)
 endif()
